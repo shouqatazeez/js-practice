13eeae75ab8ccde9eceb4bc78aa4f5717da40870
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-12 12:20:03 +0530

    add the variables  file

--- Basics/variables.js
+++ Basics/variables.js
@@ -1,3 +1,11 @@
-let Num = 10
-Num = 90
-console.log(Num);
+let num = 10
+num = 90
+console.log(num);
+
+const keyValue = 125
+// keyValue = 25  // once we decalre the value in the with const then we can't modify it agian  
+console.log(keyValue);
+
+var value = 67; // perfer not to use var due to scope issues  use let only it is the best approach
+console.log(value);
+
