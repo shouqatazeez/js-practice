e69bb2ff2609fc6700f6c84c8059fd989b2c786c
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-13 17:40:02 +0530

    add the datatypes file

---
+++ Basics/dataTypes.js
@@ -0,0 +1,17 @@
+
+/*
+let num = 1 //this is the int data type
+
+let name = "shouqat" // we represent the string data type with the "" 
+
+let login = true // this is boolean type in which it has only 2 they are true or false 
+
+null => it is the standalone funtion it is type is "object"
+
+undefine => it is of the type "undefined" it is also the standalone function means only one possible value i.e undefined only one
+
+*/ 
+
+let name = "shouqat"
+console.log(typeof name); 
+
