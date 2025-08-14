22413750e5209580c030fc1870e5bb5a8b0a6a0d
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-14 20:44:14 +0530

    add the funtions in the strings

--- Basics/strings.js
+++ Basics/strings.js
@@ -2,3 +2,16 @@ let name = new String("shouqat")
 
 console.log(name);
 console.log(name[3]);
+console.log(name.toUpperCase());
+console.log(name.charAt('2'));
+console.log(name.indexOf('o'));
+
+
+const naam = "     Shouqat "
+console.log(naam);
+console.log(naam.trim());// this function is used to remove the space in the give word
+
+const rep = "mdshouqatazeez@gmail.com"
+console.log(rep.replace('mdshouqatazeez','shouqatazeezm')); // replace fun is used to replace the word with our word that we want to change 
+console.log(rep.includes('mohammad')) // it is use to whether the word is present in the above word or not
+
