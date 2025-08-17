4067e7bf94a420c411a130f4a7c9d909b0866fd1
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-17 15:54:33 +0530

    add date.js file

---
+++ Basics/date.js
@@ -0,0 +1,5 @@
+const Dater = Date();// when you just want a quick human-readable string
+const Datee = new Date();// when you want to work with dates/times programmatically
+
+console.log(Dater);
+console.log(Datee);
