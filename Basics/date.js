113e1c74dd500e91be949f3498f671d977ea5762
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-17 19:24:01 +0530

    played with some more date functions in depth

--- Basics/date.js
+++ Basics/date.js
@@ -4,10 +4,24 @@ const Datee = new Date();// when you want to work with dates/times programmatica
 console.log(Dater);
 console.log(Datee);
 
-
-
+console.log(Datee.toLocaleString());
 console.log(Datee.toDateString());
 console.log(Datee.toTimeString());
 console.log(Datee.toJSON());
+
+// The type of date is object
+
+console.log(typeof Datee);
+
+
+let date = new Date();
+console.log(date.getFullYear());
+console.log(date.getMonth()+1);
+console.log(date.getDate());
+console.log(date.getHours())
+console.log(date.getMinutes());
+
+
+
 
 
