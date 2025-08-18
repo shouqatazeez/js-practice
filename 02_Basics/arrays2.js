acbd25f53f8e194b5cfa7e85a341a56239088bbf
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-18 19:37:38 +0530

    worked with different array methods

--- 02_Basics/arrays2.js
+++ 02_Basics/arrays2.js
@@ -3,3 +3,10 @@ const zero =["bye","goodmrg"]
 const sol=[...hero,...zero];
 console.log(sol);
 
+const arr = [1,2,3,[23,4],[2,44,55]];
+const arr1 = arr.flat(1);
+console.log(arr1);
+
+console.log(Array.isArray("shouqat"));
+console.log(Array.from("shouqat"));
+
