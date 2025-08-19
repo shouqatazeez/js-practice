fdaf5de91a94d59cb94b3889db3197bfd5afb2f9
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-19 14:00:05 +0530

    work on the differnces between the single ton and non single ton objects

---
+++ 02_Basics/objects2.js
@@ -0,0 +1,15 @@
+const user = new Object()// this is an example of the non single ton object
+user.name = "shouqat";
+const user1 = new Object();
+user1.name = "shouqat Azeez Mohammad";
+console.log(user===user1);
+
+
+
+const user2 = {//this is an example of the singleton object and it is the best approch
+    name:"shouqat",
+    age:21,
+    village:"pedapudi"
+}
+console.log(user2["name"]);
+
