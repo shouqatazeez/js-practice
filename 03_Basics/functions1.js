d1417db4630757d1320708009931cc4500aef9de
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-20 11:50:36 +0530

    understand the differnce how the program works  with and without using functions

---
+++ 03_Basics/functions1.js
@@ -0,0 +1,16 @@
+ console.log("shouqat Azeez")//with out using the functions we have to type manually if we want to print more than 
+ console.log("Mohammad");
+  console.log("shouqat Azeez")
+ console.log("Mohammad");
+
+
+
+function myname () {  // with functions we use this so that i can print so mant times so that we can't type manually 
+
+ console.log("shouqat Azeez")
+ console.log("Mohammad");
+
+
+}
+ myname();
+  myname();
