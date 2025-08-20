08e602daacfede9330aaf55a73bc1ed672e01502
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-20 11:16:44 +0530

    add a talk about the json

---
+++ 02_Basics/objects3.js
@@ -0,0 +1,15 @@
+const name = {
+    fullname:"shouqat Azeez",
+    age:21,
+    village:"pedapudi"
+}
+
+const{fullname} = name;
+console.log(fullname);
+
+
+// {
+//     "name" :"shouqat",
+//     "age":21,
+//     "village":"pedapudi"
+// }
