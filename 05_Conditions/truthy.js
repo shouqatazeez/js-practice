bef99fb7e53bfa03e32d76e36882a0f4226f56a5
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-23 16:44:10 +0530

    know about the truthy and the false values in detaild and how it works

---
+++ 05_Conditions/truthy.js
@@ -0,0 +1,20 @@
+const email = "mdshouqatazeez@gmail.come"
+const emaill = "" // if we take the " " it will give the else statement  console.log("you have't enter the email value")
+const emailvalue = [1, 3, 5, 6] //if we take the arrys value like  [1, 3, 5, 6] it will returnt the  console.log("you have given the email value");
+
+
+if (emailvalue) {
+    console.log("you have given the email value");
+
+}
+else {
+    console.log("you have't enter the email value");
+
+}
+
+//now let us discuss about the fasly values in js this are the fasly values means
+
+// false , 0 ,-0, BigInt 0n,"", null,undefined, NaN
+
+
+// truth values are "0", 'false', " " , [] , {},function() 
