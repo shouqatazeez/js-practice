aa4c947c4a7e694e644f035efc358dc3b497390d
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-24 11:22:53 +0530

    work with while and do while loops

---
+++ 06_Loops/whileloop.js
@@ -0,0 +1,17 @@
+let arr = ["shouqat", "Shouqat Azeez", "Mohammad"]
+let i = 0;
+
+while (i < arr.length) {
+    console.log(arr[i]);
+    i++;
+
+}
+
+
+let value = 0;
+do {
+    console.log(value);
+
+    value++;
+
+} while (value <= 10);
