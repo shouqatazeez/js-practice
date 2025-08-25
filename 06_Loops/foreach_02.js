9d01b6f5957e49b2b65ba5b2b89ea757bed69030
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-25 17:51:07 +0530

    know about the foreach loop in depth

---
+++ 06_Loops/foreach_02.js
@@ -0,0 +1,13 @@
+const valuess = ["shouqat", "Mohammad", "Al-Azeez"]
+const val = valuess.forEach(function (val) {// if we decalre the variables it will print along with the undefined value so better not to use the ***const val = valuess.forEach(function (val)***
+    console.log(val);
+
+})
+console.log(val);
+
+const values = ["shouqat", "Mohammad", "Al-Azeez"]// for each will not return the values
+values.forEach(function (val) {
+    console.log(val);
+
+})
+
